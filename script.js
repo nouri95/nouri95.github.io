1b5230a201f5a5ff8565bbc002d6550c264fafc5
nouri95 <86389003+nouri95@users.noreply.github.com>
2021-07-14 15:55:20 -0500

    create script.js
    replit

---
+++ script.js
@@ -0,0 +1,65 @@
+// header scrolling effect
+$(window).on('scroll', function(){
+	if($(window).scrollTop()){
+      $('header').addClass('nav-show');
+		  
+	} 
+	else{
+		$('header').removeClass('nav-show');
+	}
+	   
+})
+
+//hamburger
+const navSlide = () => {
+	 const hamburger = document.querySelector(".hamburger");
+	 const navbar = document.querySelector(".nav-bar");
+	 const navLinks = document.querySelectorAll(".nav-bar li");
+
+     hamburger.onclick = () => {
+		
+	 navbar.classList.toggle("nav-active");
+		 
+      //Animation links
+	 navLinks.forEach((link, index) => {
+		if (link.style.animation) {
+			link.style.animation = "";
+		} else {
+			link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7+1}s`;
+		   }
+		});
+	  //hamburger animation
+	 hamburger.classList.toggle("toggle");
+    }
+	 
+	}
+
+window.onload = () => navSlide();
+
+
+//material contact form animation
+$('.contact-form').find('.form-control').each(function() {
+  var targetItem = $(this).parent();
+  if ($(this).val()) {
+    $(targetItem).find('label').css({
+      'top': '10px',
+      'fontSize': '14px'
+    });
+  }
+})
+$('.contact-form').find('.form-control').focus(function() {
+  $(this).parent('.input-block').addClass('focus');
+  $(this).parent().find('label').animate({
+    'top': '10px',
+    'fontSize': '14px'
+  }, 300);
+})
+$('.contact-form').find('.form-control').blur(function() {
+  if ($(this).val().length == 0) {
+    $(this).parent('.input-block').removeClass('focus');
+    $(this).parent().find('label').animate({
+      'top': '25px',
+      'fontSize': '18px'
+    }, 300);
+  }
+})
